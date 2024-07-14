b4487d68ba3347e59e81195cbbfde582a9d4bec1
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2024-07-14 18:31:12 +0530

    joi schema validation changed

--- schema/joiSchemas.js
+++ schema/joiSchemas.js
@@ -1,12 +1,15 @@
 const Joi = require("joi");
 
-const registerSchema = Joi.object({
+const emailSchema = Joi.object({
   email: Joi.string().email().required(),
 });
+// const registerSchema = Joi.object({
+//   email: Joi.string().email().required(),
+// });
 
-const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.number().required(),
-});
+// const loginSchema = Joi.object({
+//   email: Joi.string().email().required(),
+//   password: Joi.number().required(),
+// });
 
-module.exports = { registerSchema, loginSchema };
+module.exports = { emailSchema };
