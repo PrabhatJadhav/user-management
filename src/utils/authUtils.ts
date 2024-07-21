c1c631a6cdc262a6813a8e0486d2b8f7e8122ec2
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2024-07-21 18:32:27 +0530

    try catch added to auth utility functions

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -1,39 +1,73 @@
 import jwt from "jsonwebtoken";
-import { JwtTokenPayload } from "../model/jwtTokenPayload.mode";
+import { JwtTokenPayload } from "../model/jwtTokenPayload.model";
 require("dotenv").config();
 
 export const generateToken = (payload: JwtTokenPayload) => {
   if (process.env.ACCESS_TOKEN_SECRET) {
-    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: process.env.TOKEN_EXPIRY,
-    });
+    try {
+      return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.TOKEN_EXPIRY,
+      });
+    } catch (err) {
+      throw err;
+    }
   }
 };
 
 export const generateRefreshToken = (payload: JwtTokenPayload) => {
   if (process.env.REFRESH_TOKEN_SECRET) {
-    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    });
+    try {
+      return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      });
+    } catch (err) {
+      throw err;
+    }
   }
 };
 
-export const verifyToken = (token: string) => {
+export const verifyToken = (token: string): JwtTokenPayload | null => {
   if (process.env.ACCESS_TOKEN_SECRET) {
     try {
-      return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      let decodedResult: JwtTokenPayload = jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET
+      ) as JwtTokenPayload;
+
+      if (decodedResult?.userId) {
+        return decodedResult;
+      } else {
+        return null;
+      }
     } catch (err) {
+      console.log("verifyToken catch", err);
       return null;
     }
+  } else {
+    return null;
   }
 };
 
-export const verifyRefreshToken = (token: string) => {
+export const verifyRefreshToken = (
+  refreshToken: string
+): JwtTokenPayload | null => {
   if (process.env.REFRESH_TOKEN_SECRET) {
     try {
-      return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+      let decodedResult: JwtTokenPayload = jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+      ) as JwtTokenPayload;
+
+      if (decodedResult?.userId) {
+        return decodedResult;
+      } else {
+        return null;
+      }
     } catch (err) {
+      console.log("verifyRefreshToken catch", err);
       return null;
     }
+  } else {
+    return null;
   }
 };
