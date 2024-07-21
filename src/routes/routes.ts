377bd52d2029f57e24c8fa229d2d46592fc501c5
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2024-07-21 18:34:13 +0530

    routes for testing access token and generating access token from refresh token added

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,10 +2,12 @@ import {
   customerRegister,
   customerLogin,
   verifyOtp,
+  getRefreshToken,
 } from "../controller/authController";
 const express = require("express");
 const router = express.Router();
 import "../cron/otpCleanupCron";
+import { authenticateToken } from "../middleware/verifyTokens";
 
 // Test endpoints
 
@@ -15,6 +17,12 @@ router.get("/test-api", (req: any, res: any) => {
     .send({ data: "Hello, this is test api welcome to port 8080" });
 });
 
+router.get("/test-auth-access-api", authenticateToken, (req: any, res: any) => {
+  console.log("auth req.body", req.body);
+  console.log("auth req.user", req.user);
+  res.status(200).send({ data: "Access Provided!" });
+});
+
 // User Login related endpoints
 
 router.post("/register", customerRegister);
@@ -22,5 +30,7 @@ router.post("/register", customerRegister);
 router.post("/login", customerLogin);
 
 router.post("/verify-email-otp", verifyOtp);
+
+router.post("/generate-refresh-token", getRefreshToken);
 
 export { router };
