bf31bff71842c68ee7eb2bcb2f09d7984e8f09aa
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2024-07-23 20:43:19 +0530

    watchlist controller added to add product to users watchlist

--- src/controller/watchlistController.ts
+++ src/controller/watchlistController.ts
@@ -1,57 +1,78 @@
-import { UserEmail } from "../model/userModel";
+import { Movies } from "../model/moviesModel";
+import { RegisteredUser } from "../model/userModel";
 import { UserWatchlist } from "../model/watchlistModel";
 var validator = require("validator");
 import { ApiResponse } from "../utils/apiResponse";
 
 const addToWatchlist = async (req: any, res: any, next: any) => {
-  const id = req?.params?.id;
+  const productId = req?.params?.productId;
+  const userId = req?.params?.userId;
 
-  //   Find if such product exist
-  //   const product = await UserEmail.findOne({ email: userEmail });
+  try {
+    //   Find if such product exist
 
-  if (id?.length > 10) {
-    const addToUserWatchlist = new UserWatchlist({
-      id: id,
-    });
+    const product = await Movies.findById(productId);
 
-    try {
-      addToUserWatchlist
-        .save()
-        .then((result) => {
-          res.status(200).send({
-            message: "Added to watchlist!",
-            result,
+    //   Find if such product exist
+
+    const user = await RegisteredUser.findById(userId);
+
+    if (product?._id && user?._id) {
+      const addToUserWatchlist = new UserWatchlist({
+        productId: productId,
+        userId: userId,
+      });
+
+      try {
+        addToUserWatchlist
+          .save()
+          .then((result) => {
+            res.status(200).send({
+              message: "Added to watchlist!",
+              result,
+            });
+          })
+          .catch((error) => {
+            console.log("error", error);
+            //   if (
+            //     error?.errorResponse?.code == 11000 &&
+            //     error?.errorResponse?.keyPattern?.email
+            //   ) {
+            //     res.status(500).send({
+            //       message: "Email Already Exists!",
+            //       error,
+            //     });
+
+            //     return;
+            //   }
+
+            res.status(500).send({
+              message: "Error adding to watchlist",
+              error,
+            });
           });
-        })
-        .catch((error) => {
-          console.log("error", error);
-          //   if (
-          //     error?.errorResponse?.code == 11000 &&
-          //     error?.errorResponse?.keyPattern?.email
-          //   ) {
-          //     res.status(500).send({
-          //       message: "Email Already Exists!",
-          //       error,
-          //     });
-
-          //     return;
-          //   }
-
-          res.status(500).send({
-            message: "Error adding to watchlist",
-            error,
-          });
-        });
-    } catch (e) {
-      console.log("e", e);
+      } catch (e) {
+        console.log("e", e);
+        res
+          .status(500)
+          .json(ApiResponse.success(null, "Something Went Wrong!", 500));
+      }
+    } else if (user?._id) {
       res
         .status(400)
-        .json(ApiResponse.success(null, "Something Went Wrong!", 400));
+        .json(ApiResponse.success(null, "Please provide a valid id!", 400));
+    } else if (product?._id) {
+      res.status(400).json(ApiResponse.success(null, "Invalid User!", 400));
+    } else {
+      res
+        .status(500)
+        .json(ApiResponse.success(null, "Something Went Wrong!", 500));
     }
-  } else {
+  } catch (e) {
+    console.log("e", e);
     res
-      .status(400)
-      .json(ApiResponse.success(null, "Please provide a valid id!", 400));
+      .status(500)
+      .json(ApiResponse.success(null, "Something Went Wrong!", 500));
   }
 };
 
@@ -66,8 +87,8 @@ const removeFromWatchlist = (req: any, res: any, next: any) => {
     }
 
     if (userEmail && isValidEmail) {
-      const user = new UserEmail({
-        email: userEmail,
+      const user = new Movies({
+        _id: userEmail,
       });
 
       user
