a2a41510a96bfc496ecdfd93de0bf30882074c6c
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2024-07-31 15:21:06 +0530

    swagger route added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { dbConnect } from "./db/dbConnect";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { router } from "./routes/routes";
+import { swaggerDocs } from "./swagger";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -20,6 +22,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", router);
+
+app.use("/api/v1/docs", swaggerUi?.serve, swaggerUi?.setup(swaggerDocs));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
